be8e3d4504e52a19d8a7132989359ddb3db9cb89
alastaira <alastair@playful.technology>
2019-12-01 08:58:48 +0000

    Update arduino-gesture-recogniser.ino

--- arduino-gesture-recogniser.ino
+++ arduino-gesture-recogniser.ino
@@ -8,44 +8,53 @@ void setup() {
 	Serial.begin(9600);
 
   // Initialise the sensor
-	if(!paj7620Init()) {
-    Serial.println(F("Initialisation error!")
+  int error = paj7620Init();
+	if(error) {
+    Serial.print(F("Initialisation error code: "));
+    Serial.println(error);
 	}
 	else {
-		Serial.println(F("Ready!"");
+		Serial.println(F("Ready!"));
 	}
 }
 
 void loop() {
-	uint8_t data = 0, data1 = 0, error;
-
+  // Create a variable to hold the value of any gesture recognised
+	byte gesture;
+  // Error variable holds any error code
+	int error;
   // Read Reg 0x43 of Bank 0 to get result of any recognised gesture, and store in 'data' variable
-	error = paj7620ReadReg(0x43, 1, &data);
+	error = paj7620ReadReg(0x43, 1, &gesture);
  
 	if(!error) {
-		switch (data) {
+		switch (gesture) {
 			case GES_RIGHT_FLAG:
-			  Serial.println("Right");          
+			  Serial.println(F("Right"));          
 				break;
 			case GES_LEFT_FLAG: 
-				Serial.println("Left");         
+				Serial.println(F("Left"));         
 				break;
 			case GES_UP_FLAG:
-				Serial.println("Up");        
+				Serial.println(F("Up"));        
 				break;
 			case GES_DOWN_FLAG:
-				Serial.println("Down");         
+				Serial.println(F("Down"));         
 				break;
 			case GES_FORWARD_FLAG:
-				Serial.println("Forward");
+				Serial.println(F("Forward"));
 				break;
 			case GES_BACKWARD_FLAG:		  
-				Serial.println("Backward");
+				Serial.println(F("Backward"));
 				break;
       // Library also defines GES_CLOCKWISE_FLAG GES_COUNT_CLOCKWISE_FLAG and GES_WAVE_FLAG, but I found these unreliable
 			default:
 				break;
 		}
 	}
-	delay(100);
+ else {
+    Serial.print(F("Error code: "));
+    Serial.println(error);
+ }
+ 
+  delay(100);
 }
